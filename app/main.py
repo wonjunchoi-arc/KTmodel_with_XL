075e9e3bd48ac8b12adf0f3005b5583c5dffde3c
JUN <qlszkd13@naver.com>
2024-02-05 17:01:09 +0900

    user select devices

--- app/main.py
+++ app/main.py
@@ -1,8 +1,14 @@
+import os
+import sys
+current_directory = os.path.dirname(os.path.abspath(__file__))
+parent_directory = os.path.dirname(current_directory)
+sys.path.append(parent_directory)
+
 from fastapi import FastAPI
 from pydantic import BaseModel
 from app.predict import predict_pipline
 from app.predict import __version__ as model_version
-
+import argparse
 import uvicorn
 
 
@@ -11,6 +17,7 @@ app = FastAPI()
 class StudentLogPath(BaseModel):
     Path : str
     uid : int
+    devices : str
 
 
 class PredictionOut(BaseModel):
@@ -22,12 +29,7 @@ def home():
 
 @app.post("/predict", response_model=PredictionOut)
 def predict(payload : StudentLogPath):
-    out_path =predict_pipline(payload.Path, payload.uid)
+    out_path =predict_pipline(payload.Path, payload.uid, payload.devices)
     return {"output_path": out_path}
-
-
-
-# if __name__ == '__main__':
-#     uvicorn.run(app)
 
 
--- app/predict.py
+++ app/predict.py
@@ -1,91 +1,95 @@
 import os
+import sys
+current_directory = os.path.dirname(os.path.abspath(__file__))
+parent_directory = os.path.dirname(current_directory)
+sys.path.append(parent_directory)
+from fastapi import HTTPException
 import pandas as pd
 import tensorflow as tf
 from preprocess.data_utils import get_evalmask_token
 from models.model_for_kt import TFTransfoXLMLMHeadModel
 from pathlib import Path
 from transformers import TransfoXLConfig
+import argparse
 
-BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
-
-
-config_xl = TransfoXLConfig.from_pretrained('{}/save_model/5ep_400mem_concepts.ckpt/config.json'.format(BASE_DIR))
-
-model = TFTransfoXLMLMHeadModel(config=config_xl)
-model.load_weights('{}/save_model/5ep_400mem_concepts.ckpt/my_checkpoint'.format(BASE_DIR))
 
 __version__="0.1.0"
+BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 
-# model = TFTransfoXLMLMHeadModel(config=config_xl)
-# with open(f"{BASE_DIR}/trained_pipline-{__version__}/my_checkpoint",'rb')as f:
-#     model.load_weights(f)
+def predict_pipline(path,uid,devices):
 
+    device = "/cpu:0" if device == "cpu" else "/gpu:0"
+    try:
+        with tf.device(device):
 
-# csv 파일을 입력으로 받아서 concepts열과 reponse열을 선택하고 concepts를 쭉 나열 한 뒤 학생이 현재 지도에 맞춰 푼 문제를 response라 하여 제공 
-#predict는 학생이 아직 풀지 않은 즉 마스킹 된 값들로 만들어서 예측할 수 있도록 하자 
-def predict_pipline(path,uid):
-        
-    
-    csv_file_path = path # 이 값을 사용자가 넣을 수 있도록 하자
+            config_xl = TransfoXLConfig.from_pretrained('{}/save_model/5ep_400mem_concepts.ckpt/config.json'.format(BASE_DIR))
 
-    df= pd.read_csv(csv_file_path)
+            model = TFTransfoXLMLMHeadModel(config=config_xl)
+            model.load_weights('{}/save_model/5ep_400mem_concepts.ckpt/my_checkpoint'.format(BASE_DIR))
 
-    question_list = df.loc[df['uid'] == uid, 'questions']
-    concepts_list = df.loc[df['uid'] == uid, 'concepts']
-    responses_list = df.loc[df['uid'] == uid, 'responses']
+            csv_file_path = path # 이 값을 사용자가 넣을 수 있도록 하자
 
-    question_list = [int(x) for x in question_list.values[0].split(',')]
-    concepts_list = [int(x) for x in concepts_list.values[0].split(',')]
-    responses_list = [int(x) for x in responses_list.values[0].split(',')]
+            df= pd.read_csv(csv_file_path)
 
-    masked_R, labels = get_evalmask_token(responses_list,config_xl.mask_token, config_xl.eos_token)
+            question_list = df.loc[df['uid'] == uid, 'questions']
+            concepts_list = df.loc[df['uid'] == uid, 'concepts']
+            responses_list = df.loc[df['uid'] == uid, 'responses']
 
+            question_list = [int(x) for x in question_list.values[0].split(',')]
+            concepts_list = [int(x) for x in concepts_list.values[0].split(',')]
+            responses_list = [int(x) for x in responses_list.values[0].split(',')]
 
-
-    # 여기서 읽어온 데이터를 딕셔너리키 가지고 변환해서 ip mapping 하고 
+            masked_R, labels = get_evalmask_token(responses_list,config_xl.mask_token, config_xl.eos_token)
 
 
-    new_shape = (1, -1)  # 나머지 차원은 자동으로 계산됨
 
-    qseq_reshaped = tf.reshape(question_list, new_shape)
-    cseq_reshaped = tf.reshape(concepts_list, new_shape)
-    masked_R_reshaped = tf.reshape(masked_R, new_shape)
-    labels_reshaped = tf.reshape(labels, new_shape)
+            # 여기서 읽어온 데이터를 딕셔너리키 가지고 변환해서 ip mapping 하고 
 
 
-    qseq_transposed = tf.transpose(qseq_reshaped)
-    cseq_transposed = tf.transpose(cseq_reshaped)
-    masked_R_transposed = tf.transpose(masked_R_reshaped)
-    labels_transposed = tf.transpose(labels_reshaped)
+            new_shape = (-1, 1)  # 나머지 차원은 자동으로 계산됨
 
-    
-
-    if config_xl.mode == 'concepts':
-        predict_dataset = tf.data.Dataset.from_tensor_slices(
-        (cseq_transposed, masked_R_transposed,labels_transposed))
-    else:
-         predict_dataset = tf.data.Dataset.from_tensor_slices(
-        (qseq_transposed, masked_R_transposed,labels_transposed))
-
-    predict_dataset =predict_dataset.batch(config_xl.tgt_len)
-
-    mems =None
-    predictions = []
-    for input_data, input_data2, input_target in predict_dataset:
-            outputs = model(concepts=input_data, responses=input_data2, labels=input_target, mems=mems)
-            logit = outputs.loss
-            mems = outputs.mems
+            qseq_reshaped = tf.reshape(question_list, new_shape)
+            cseq_reshaped = tf.reshape(concepts_list, new_shape)
+            masked_R_reshaped = tf.reshape(masked_R, new_shape)
+            labels_reshaped = tf.reshape(labels, new_shape)
             
-            reshape = tf.reshape(logit, [-1, config_xl.R_vocab_size])
-            predicted_labels = tf.argmax(reshape, axis=1)
-            predictions.append(predicted_labels.numpy().tolist())
 
-    flattened_list = [item for sublist in predictions for item in sublist]
+            # qseq_transposed = tf.transpose(qseq_reshaped)
+            # cseq_transposed = tf.transpose(cseq_reshaped)
+            # masked_R_transposed = tf.transpose(masked_R_reshaped)
+            # labels_transposed = tf.transpose(labels_reshaped)
 
-    df = pd.DataFrame({'Question':question_list,'Concepts':concepts_list,'Responses':flattened_list})
-    save_dir = '{}/{}.csv'.format(BASE_DIR, uid)
+            
 
-    df.to_csv(save_dir, index=False)
+            if config_xl.mode == 'concepts':
+                predict_dataset = tf.data.Dataset.from_tensor_slices(
+                (cseq_reshaped, masked_R_reshaped,labels_reshaped))
+            else:
+                predict_dataset = tf.data.Dataset.from_tensor_slices(
+                (qseq_reshaped, masked_R_reshaped,labels_reshaped))
 
-    return save_dir
+            predict_dataset =predict_dataset.batch(config_xl.tgt_len)
+            # predict_dataset =predict_dataset.batch(140)
 
+            mems =None
+            predictions = []
+            for input_data, input_data2, input_target in predict_dataset:
+            
+                    outputs = model(concepts=tf.transpose(input_data), responses=tf.transpose(input_data2), labels=tf.transpose(input_target), mems=mems)
+                    logit = outputs.logit
+                    mems = outputs.mems
+                    
+                    reshape = tf.reshape(logit, [-1, config_xl.R_vocab_size])
+                    predicted_labels = tf.argmax(reshape, axis=1)
+                    predictions.append(predicted_labels.numpy().tolist())
+
+            flattened_list = [item for sublist in predictions for item in sublist]
+
+            df = pd.DataFrame({'Question':question_list,'Concepts':concepts_list,'Responses':flattened_list})
+            save_dir = '{}/{}.csv'.format(BASE_DIR, uid)
+
+            df.to_csv(save_dir, index=False)
+        return save_dir
+
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
